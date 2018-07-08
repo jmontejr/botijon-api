d6a4b85bf6ce34ba5b351ec4d38e4f3343da8790
jmontejr <josecmontejr@gmail.com>
2018-07-08 19:45:12 -0300

    Teste insercao 6

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createPool({
     // user: 'root',
     // password: '',
     // database: 'botijon'
-    host: 'sql31.hostinger.com.br/localhost',
+    host: 'sql31.hostinger.com.br',
     user: 'u834126065_botij',
     password: 'BotijOnAppKey123',
     database: 'u834126065_botij'
