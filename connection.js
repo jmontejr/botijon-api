484e194ab6ccad07a42cf56dca5299c88a9da9c2
jmontejr <josecmontejr@gmail.com>
2018-07-08 19:49:58 -0300

    Teste insercao 7

--- connection.js
+++ connection.js
@@ -1,14 +1,23 @@
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
+    // Local
     // host: 'localhost',
     // user: 'root',
     // password: '',
     // database: 'botijon'
-    host: 'sql31.hostinger.com.br',
-    user: 'u834126065_botij',
+
+    // Hostinger
+    // host: 'sql31.hostinger.com.br',
+    // user: 'u834126065_botij',
+    // password: 'BotijOnAppKey123',
+    // database: 'u834126065_botij'
+
+    // 000Webhost
+    host: 'databases-auth.000webhost.com',
+    user: 'id2500549_botijonadmin',
     password: 'BotijOnAppKey123',
-    database: 'u834126065_botij'
+    database: 'id2500549_botijondb'
 });
 
 module.exports = connection;
